621bff9c6c6be6bab26c4ecf4258d724ea480b0e
Ted Heikkila <ted.heikkila@gmail.com>
2021-06-20 16:44:13 -0500

    temp fix for save btn; moving to moment.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,38 @@
 // custom script for daily planner, TWH, 6-19-21
 
-// plan:
-    //1. need to use jQuery and moment.js to determine how bg colors on time rows (if structure)
-        //less than current time then gray; current = red; greater than = green (on page refresh)
-    //2. need to capture text entered as input in text area
-    //3. need forEach click event for every save btn (one function)
-    //4. capture this text and JSON stringify set into local storage
-    //5. get from local storage and render onto hour type into; input fields required for each row
+//global vars
 
-// selectors/global vars: row bg color; input text; local storage variables; time at moment X
-//                          save btn
+    // where user enters in events (the textarea)
+    var dailyEvent = $('.form-input');
+    // this is colored section (the past, present, future areas)
+    var savedEvent = $('.input-group');
+    // saveBtn (blue)
+    var saveBtn = $('.saveBtn');
+    // string array for times (to associate with text input)
+    var timesString = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+    // military time array (to associate current time and moment.js for changing bg color)
+    var timesNumbers = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-// challenges: changing row bg color based on current time; setting/getting from local storage
+// loops through timesString array and attaches custom attribute
+function attachTimes() {
 
-// red = past; blue = present; green = future hour
+    // for-loop to iterate through the timesString array
+    for (var i = 0; i < timesString.length; i++) {
+         // Assign style to the button
+        dailyEvent.addClass('time-class');
+        // Assign the text area to the data-time attribute
+        dailyEvent.attr('time-attr', timesString[i]);
+    }
 
-// changing bg color plan:
-    // create lets using jQuery syntax for 9, 10, 11a selectors; going to have to add specific 
-    // class values in the HTML; set these variables as integer values (military time) then put into an array
-    // use moment js to grab the current hour time in interger format (military time)
-    // in an if statement go through the array and compared which values are greater
-    // than the moment time, equal, and less than (by their index value)
-    // then finally change the bg color of each using style property jQuery
+}
 
-// save text input plan:
-    // JSON stringify the user's text value upon a forEach click event for the save button(s)
-    // store in local storage
-    // parse out using JSON and render onto the page. if content is already there then append the 
-    // string to pre-existing text 
-    // after page refresh the rendered content should persist
-
-// today's date in the following format: Saturday, July 4th 2011, 6:30 (top of page)
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
-
-// capturing and saving item to page (also put into local storage)
-var dailyEvent = $('.form-input');
-
-// need an append location for saved events
-var savedEvent = $('.input-group');
-
-// global var saveBtn
-var saveBtn = $('.saveBtn');
-
-// call render function on page refresh
+// call to render on page refresh
 renderLastEvent()
  
-//saves event to page using local storage and append
+// Delegate event listener to saveBtn; isolate to specific section being typed in using code above
 function saveEvent(event) {
-    event.preventDefault()
 
-    var saveBtnClicked = $(event.target);
+    event.preventDefault()
 
     var typedEvent = $("input[name='form-input']").val();
 
@@ -61,42 +43,74 @@ function saveEvent(event) {
         return;
       }
 
-      saveBtnClicked.append(typedEvent)
-      
-    //   savedEvent.append(typedEvent)
+    var typedEventItem = $(
+        '<li class="flex-row justify-space-between align-center p-2 bg-light text-dark">'
+      );
+
+    typedEventItem.text(typedEvent);
     
+    // get time from clicked time's ave button w/ data-time attribute and use it for saving specific event
+    // typedEvent.text($(event.target).attr('time-attr'));
+  
+    // print to the page
+    savedEvent.append(typedEventItem)
 
     // set input to local storage using "key" as key
     localStorage.setItem("typedEvent", typedEvent);
 
-    // print to the page
-    dailyEvent.append(typedEvent);
+    // if (savedEvent===null) {
+    //put already saved events into local storage
+    localStorage.setItem("savedEvent", savedEvent);
+    // } else localStorage.setItem("savedEvent", JSON.stringify(savedEvent));
 
-    renderLastEvent()
+
+    // renderLastEvent()
 
 }
 
-// event delegation for every save button on form
-dailyEvent.on('click', '.saveBtn', saveEvent);
+saveBtn.on('click', saveEvent);
 
 // get items entered from local storage, so events persist through browser reload events
-$(window).bind('beforeunload',function(){
+// $(window).bind('beforeunload',function(){
 
-    renderLastEvent()
+//     renderLastEvent()
 
-   return 'Are you sure you want to leave?';
+//    return 'Are you sure you want to leave?';
 
-});
+// });
 
 //get items from local storage
 function renderLastEvent() {
     var renderEvent= localStorage.getItem("typedEvent");
 
+    // if (savedEvent===null) {
+    var renderSaved = localStorage.getItem("savedEvent");
+    // } else var renderSaved = JSON.parse(localStorage.getItem("savedEvent"));
+
+    
+
      // prints to the page
      savedEvent.append(renderEvent);
+     savedEvent.append(renderSaved);
 }
 
 
+//moment.js section:
+
+// red = past; yellow = present; green = future; blue = save
+
+// changing bg color plan:
+    // create lets using jQuery syntax for 9, 10, 11a selectors; going to have to add specific 
+    // class values in the HTML; set these variables as integer values (military time) then put into an array
+    // use moment js to grab the current hour time in interger format (military time)
+    // in an if statement go through the array and compared which values are greater
+    // than the moment time, equal, and less than (by their index value)
+    // then finally change the bg color of each using style property jQuery
+
+
+// today's date in the following format: Saturday, July 4th 2011, 6:30 (top of page)
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 
 
 
